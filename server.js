821e4e961499ee5b7acb65beac8f869c8b183612
RohanPunjani <rohanpunjani32@gmail.com>
2020-08-02 09:32:56 +0530

    Server listening ports

--- server.js
+++ server.js
@@ -121,5 +121,6 @@ io.on('connection', socket => {
     });
 })
 
-
-server.listen(3000)
+server.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
