5360fa1b17e6a435bacb3d29acd1386e70c5837b
Rohan Punjani <rohanpunjani32@gmail.com>
2021-06-21 12:30:20 +0530

    Closing DB

--- db.js
+++ db.js
@@ -1,17 +1,17 @@
-const { MongoClient } = require("mongodb");
-const mongoose = require('mongoose');
-require('dotenv/config');
+// const { MongoClient } = require("mongodb");
+// const mongoose = require('mongoose');
+// require('dotenv/config');
 
-const UserSchema = mongoose.Schema({
-    id: {
-    }
+// const UserSchema = mongoose.Schema({
+//     id: {
+//     }
         
-})
+// })
 
 
-mongoose.connect(process.env.MONGO_URI,
-    {useNewUrlParser:true},()=>{
-        console.log("Connected to the db")
-    }
-  )
+// mongoose.connect(process.env.MONGO_URI,
+//     {useNewUrlParser:true},()=>{
+//         console.log("Connected to the db")
+//     }
+//   )
 
