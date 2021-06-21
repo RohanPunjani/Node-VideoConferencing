de28fb2d13be464b7e5c9c8c601ddc56e3d11995
Rohan Punjani <rohanpunjani32@gmail.com>
2021-06-21 12:49:15 +0530

    Peerjs port

--- public/js/script.js
+++ public/js/script.js
@@ -8,7 +8,7 @@ const socket = io("/");
 const videoGrid = document.getElementById("video-grid");
 const myPeer = new Peer(USER_ID, {
   host: "/",
-  port: "3001",
+  port: "443",
 });
 const myVideo = document.createElement("video");
 myVideo.muted = true;
